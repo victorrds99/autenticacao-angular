1906b066b1d4300849ea1a89f302f78c70d3aa7a
Rodrigues De Souza <vrodriguess@indracompany.com>
2021-08-17 16:24:12 -0300

    Adicionamos o botão Hora exata, e colocamos um relogio em tempo em real.

--- src/app/servicos/data-hora.service.ts
+++ src/app/servicos/data-hora.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, interval } from 'rxjs';
+import { map, share } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataHoraService {
 
+  dataHora = new BehaviorSubject(this.obterDataHoraAtual());
+  
+  dataHoraTempoReal = interval(500).pipe(
+    map(()=> this.obterDataHoraAtual()), share()
+  )
+  
+
   constructor() { }
+
+  obterDataHoraAtual() {
+    const data = new Date();
+    return data.toLocaleString('pt-BR');
+  }
+  atualizarDataHora() {
+    this.dataHora.next(this.obterDataHoraAtual())
+  }
+  
 }
